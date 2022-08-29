ce3f5a2d9c6584ccd06f61738da006dd3f950e3a
Zeeshan <zeeshanrehman234@gmail.com>
2022-08-29 16:10:46 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let togglebtn = document.getElementById("toggle")
+let nav = document.getElementById("nav")
+
+
+
+togglebtn.addEventListener("click", ()=>{
+    nav.classList.toggle("active")
+    
+})
